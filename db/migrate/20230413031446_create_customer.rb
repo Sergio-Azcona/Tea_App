fad125521784d44e35534a214baf58dea7a97c23
Sergio Azcona <sergioazcona@Sergios-MBP.home>
2023-04-12 23:28:43 -0400

    Schema & model relationships tests complete

---
+++ db/migrate/20230413031446_create_customer.rb
@@ -0,0 +1,14 @@
+class CreateCustomer < ActiveRecord::Migration[5.2]
+  def change
+    create_table :customers do |t|
+      t.string :first_name
+      t.string :last_name
+      t.string :email
+      t.string :street_address
+      t.string :state
+      t.integer :zip_code
+    
+      t.timestamps
+    end
+  end
+end
